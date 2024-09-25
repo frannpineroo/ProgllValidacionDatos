6420440a8e9f1bffe8f03e5b2d92562dea552eab
Francisco Piñero <frannpinero@hotmail.com>
2024-09-25 16:14:35 -0300

    Agregar archivos de proyecto.

---
+++ ProgllValidacionDatos/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ProgllValidacionDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPatente = new System.Windows.Forms.Label();
+            this.lblCuil = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtPatente = new System.Windows.Forms.TextBox();
+            this.txtCuil = new System.Windows.Forms.TextBox();
+            this.buttValidar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPatente
+            // 
+            this.lblPatente.AutoSize = true;
+            this.lblPatente.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPatente.Location = new System.Drawing.Point(12, 48);
+            this.lblPatente.Name = "lblPatente";
+            this.lblPatente.Size = new System.Drawing.Size(86, 25);
+            this.lblPatente.TabIndex = 0;
+            this.lblPatente.Text = "Patente";
+            // 
+            // lblCuil
+            // 
+            this.lblCuil.AutoSize = true;
+            this.lblCuil.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCuil.Location = new System.Drawing.Point(12, 118);
+            this.lblCuil.Name = "lblCuil";
+            this.lblCuil.Size = new System.Drawing.Size(49, 25);
+            this.lblCuil.TabIndex = 1;
+            this.lblCuil.Text = "Cuil";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(186, 49);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(60, 24);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "label3";
+            // 
+            // txtPatente
+            // 
+            this.txtPatente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPatente.Location = new System.Drawing.Point(12, 76);
+            this.txtPatente.Name = "txtPatente";
+            this.txtPatente.Size = new System.Drawing.Size(124, 26);
+            this.txtPatente.TabIndex = 3;
+            // 
+            // txtCuil
+            // 
+            this.txtCuil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCuil.Location = new System.Drawing.Point(12, 146);
+            this.txtCuil.Name = "txtCuil";
+            this.txtCuil.Size = new System.Drawing.Size(124, 26);
+            this.txtCuil.TabIndex = 4;
+            // 
+            // buttValidar
+            // 
+            this.buttValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttValidar.Location = new System.Drawing.Point(84, 208);
+            this.buttValidar.Name = "buttValidar";
+            this.buttValidar.Size = new System.Drawing.Size(85, 33);
+            this.buttValidar.TabIndex = 5;
+            this.buttValidar.Text = "Validar";
+            this.buttValidar.UseVisualStyleBackColor = true;
+            this.buttValidar.Click += new System.EventHandler(this.buttValidar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttValidar);
+            this.Controls.Add(this.txtCuil);
+            this.Controls.Add(this.txtPatente);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblCuil);
+            this.Controls.Add(this.lblPatente);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPatente;
+        private System.Windows.Forms.Label lblCuil;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtPatente;
+        private System.Windows.Forms.TextBox txtCuil;
+        private System.Windows.Forms.Button buttValidar;
+    }
+}
+
---
+++ ProgllValidacionDatos/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ProgllValidacionDatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttValidar_Click(object sender, EventArgs e)
+        {
+            string patente = txtPatente.Text;
+            string cuil = txtCuil.Text;
+
+            // Verificar si los TextBoxes están vacíos antes de validar
+            if (string.IsNullOrWhiteSpace(patente))
+            {
+                lblResultado.Text = "Por favor, ingrese una patente.";
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cuil))
+            {
+                lblResultado.Text = "Por favor, ingrese un CUIL.";
+                return;
+            }
+
+            if (ValidarPatente(patente))
+            {
+                lblResultado.Text = "La patente es válida.";
+            }
+            else
+            {
+                lblResultado.Text = "La patente no es válida.";
+            }
+
+            string dni = ExtraerDniDeCuil(cuil);
+
+            if (dni != null)
+            {
+                lblResultado.Text = "El DNI extraído es: " + dni;
+            }
+            else
+            {
+                lblResultado.Text = "El fórmato del CUIL no es válido";
+            }
+        }
+
+        static bool ValidarPatente(string patente)
+        {
+            string pattern = @"^[A-Z]{2}\d{3}[A-Z]{2}$|^[A-Z]{3}\d{3}$";
+            return Regex.IsMatch(patente, pattern);
+        }
+
+        static string ExtraerDniDeCuil(string cuil)
+        {
+            string pattern = @"^\d-\d{8}-\d$";
+            if (Regex.IsMatch(cuil, pattern))
+            {
+                return cuil.Split('-')[1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
